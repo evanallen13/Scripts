f9428dd66e9d1f9db69c8fda8e29ace41b40c483
evanallen13 <evanallen13@gmail.com>
2021-03-13 11:04:08 -0800

    First

---
+++ AddTags.ps1
@@ -0,0 +1,3 @@
+﻿$tags = @{"CostCenter"="00123"; "Environment"="Production"}
+$resourceGroup = Get-AzResourceGroup -Name CentOs
+Update-AzTag -ResourceId $resourceGroup.ResourceId -Tag $tags -Operation Merge
